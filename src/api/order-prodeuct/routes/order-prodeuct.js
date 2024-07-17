ec4905be62a7fda18ffcd4febbd591a3c2235e77
Ogabek <orziyevogabek67@gmail.com>
2024-07-17 12:38:29 +0500

    first commit

---
+++ src/api/order-prodeuct/routes/order-prodeuct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-prodeuct router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::order-prodeuct.order-prodeuct');
---
+++ src/api/order-prodeuct/services/order-prodeuct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-prodeuct service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::order-prodeuct.order-prodeuct');
